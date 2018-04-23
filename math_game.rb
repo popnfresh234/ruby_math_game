8c3aa5aab027b3827fd9926c67156c55e4ec2bee
Alex Holliday <ajhollid@gmail.com>
2018-04-23 22:36:07 +0000

    refactored

--- math_game.rb
+++ math_game.rb
@@ -19,30 +19,37 @@ def display_score(player_a, player_b)
     "#{player_a.name} #{player_a.lives}/3 vs #{player_b.name} #{player_b.lives}/3"
 end
 
-puts "Lets play MATH_GAME!!!"
 
-players = []
-p1 = Player.new('Player One')
-p2 = Player.new('Player Two')
-players << p1
-players << p2
-
-puts "---- First Turn ----"
-
-loop do
-    round(p1, p2)
-    break if p1.lives == 0
-    puts "---- New Turn ----"
-    round(p2, p1)
-    break if (p2.lives == 0)
-    puts "---- New Turn ----"
+def setup_game
+    players = []
+    p1 = Player.new('Player One')
+    p2 = Player.new('Player Two')
+    players << p1
+    players << p2
+    puts "Lets play MATH_GAME!!!"
+    puts "---- First Turn ----"
+    run_game(players)
 end
 
-players.sort_by! do |player|
-    -player.lives
+def run_game(players)
+
+    loop do
+        round(players[0], players[1])
+        break if players[0].lives == 0
+        puts "---- New Turn ----"
+        round(players[1], players[0])
+        break if (players[1].lives == 0)
+        puts "---- New Turn ----"
+    end
+    
+    players.sort_by! do |player|
+        -player.lives
+    end
+    
+    puts "----   -----   ----"
+    puts "#{players[0].name} wins with a score of #{players[0].lives}/3"
+    puts "---- GAME OVER ----"
+    puts "Good bye!"
 end
 
-puts "----   -----   ----"
-puts "#{players[0].name} wins with a score of #{players[0].lives}/3"
-puts "---- GAME OVER ----"
-puts "Good bye!"
+setup_game
